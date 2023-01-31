2dfa8c7c5b423a3eef2762099b13c4083e34ef67
Victoria Bel <victoriab@moil.noacs.io>
2023-01-31 19:36:46 +0100

    More intro + some method

--- main.R
+++ main.R
@@ -1,3 +1,11 @@
 
-
+library(rofi)
+data <- rofi::import_data(test = TRUE)
+names <- c("swetrau","fmp","atgarder","problem","kvalgranskning2014.2017")
+names(data) <- names
 combined.dataset <- rofi::merge_data(data)
+
+## Create OFI column
+combined.dataset$ofi <- rofi::create_ofi(combined.dataset)
+
+
