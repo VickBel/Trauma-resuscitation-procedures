3449d86c788016188978671f4337a93d77a29294
Victoria Bel <victoriab@noacs.io>
2023-02-13 05:59:14 +0100

    Made some changes based on feedback on draft 1. Added a bit more in the methodology section.

--- main.R
+++ main.R
@@ -1,10 +1,12 @@
 
-library("tidyverse")
+library(tidyverse)
 library(rofi)
 library(bookdown)
 library(dplyr)
 library(noacsr)
-
+library(ggplot2)
+library(DiagrammeR) 
+library(table1)
 
 data <- rofi::import_data(test = TRUE)
 names <- c("swetrau","fmp","atgarder","problem","kvalgranskning2014.2017")
@@ -16,8 +18,6 @@ combined.dataset <- rofi::merge_data(data)
 ## Create OFI column
 combined.dataset$ofi <- rofi::create_ofi(combined.dataset)
 combined.dataset <- clean_audit_filters(combined.dataset)
-<<<<<<< HEAD
-=======
 
 ## Separate and store cases without known outcome (OFI)
 missing.outcome <- is.na(combined.dataset$ofi)
@@ -32,27 +32,6 @@ combined.dataset <- clean_all_predictors(combined.dataset)
 ## clean Audit filters
 combined.dataset <- clean_audit_filters(combined.dataset)
 
-## Create a flow chart (Saves it as a pdf)
-
-create_flowchart(combined.dataset)
-
-
->>>>>>> f9aeffcca3686f0f964ca2c3d343ed67f6e8e62c
-
-## Separate and store cases without known outcome (OFI)
-missing.outcome <- is.na(combined.dataset$ofi)
-combined.dataset <- combined.dataset[!missing.outcome,]
-
-## remove patients < 15 years
-combined.dataset <- combined.dataset[combined.dataset$pt_age_yrs > 14,]
-
-## Fix formating and remove wrong values like 999
-combined.dataset <- clean_all_predictors(combined.dataset)
-
-## clean Audit filters
-combined.dataset <- clean_audit_filters(combined.dataset)
 
 ## Create a flow chart (Saves it as a pdf)
-
-create_flowchart(combined.dataset)
-
+#create_flowchart(combined.dataset)
