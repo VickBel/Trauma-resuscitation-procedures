e2acdf2381b83ad10517a4e20288aa446e39f13c
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2023-04-03 23:18:28 +0200

    Edit code and create tables

--- format_table.R
+++ format_table.R
@@ -2,57 +2,62 @@
 
 format_table <- function(data) {
 
-# "res_survival"
-data$res_survival <- factor(
-  data$res_survival,
-  levels = c(1, 2),     
-  labels = c("Dead", "Alive"))
+    ## "res_survival"
+    data$res_survival <- factor(
+        data$res_survival,
+        levels = c(1, 2),     
+        labels = c("Dead", "Alive"))
 
-#"pt_Gender"              
-data$pt_Gender <- factor(
-  data$pt_Gender,
-  levels = c(2, 1),
-  labels = c("Female", "Male"))
+    ##"pt_Gender"              
+    data$pt_Gender <- factor(
+        data$pt_Gender,
+        levels = c(2, 1),
+        labels = c("Female", "Male"))
 
-# ed_emerg_proc_other
-data$ed_emerg_proc_other <- factor(
-  data$ed_emerg_proc_other,
-  levels = c(1, 2, 3, 4, 5), 
-  labels = c("Thoracic drainage",
-             "External fixation of fracture",
-             "Major fracture surgery",
-             "Surgical wound revision",
-             "Other intervention")) 
+    ## ed_emerg_proc_other
+    data$ed_emerg_proc_other <- factor(
+        data$ed_emerg_proc_other,
+        levels = c(1, 2, 3, 4, 5), 
+        labels = c("Thoracic drainage",
+                   "External fixation of fracture",
+                   "Major fracture surgery",
+                   "Surgical wound revision",
+                   "Other intervention")) 
 
-# "ed_emerg_proc" 
-data$ed_emerg_proc <- factor(
-  data$ed_emerg_proc,
-  levels = c(1, 2, 3, 4, 5, 6, 7, 8), # exclusion of 8, 99, 999 (8 - "Other intervention" removed since they are present in ed_emerg_proc_other)
-  labels = c("Thoracotomy",
-             "Laparotomy - hemostasis",
-             "Pelvic packing",
-             "Revascularization (including surgery for pulseless extremity)",
-             "Radiological intervention (Endovascular = embolization, stent, stent graft)",
-             "Craniotomy",
-             "Intracranial pressure measurement as sole intervention",
-             "OTHER INTERVENTIONS")) 
+    ## "ed_emerg_proc" 
+    data$ed_emerg_proc <- factor(
+        data$ed_emerg_proc,
+        levels = c(1, 2, 3, 4, 5, 6, 7, 8), # exclusion of 8, 99, 999 (8 - "Other intervention" removed since they are present in ed_emerg_proc_other)
+        labels = c("Thoracotomy",
+                   "Laparotomy - hemostasis",
+                   "Pelvic packing",
+                   "Revascularization (including surgery for pulseless extremity)",
+                   "Radiological intervention (Endovascular = embolization, stent, stent graft)",
+                   "Craniotomy",
+                   "Intracranial pressure measurement as sole intervention",
+                   "Other intervention")) 
+    
+    ## Merge ed_emerg_proc_other and ed_emerg_proc
+    resuscitation.procedures <- as.character(data$ed_emerg_proc)
+    index <- resuscitation.procedures == "Other intervention" & !is.na(resuscitation.procedures)
+    resuscitation.procedures[index] <- as.character(data$ed_emerg_proc_other[index])
+    data$resuscitation.procedures <- factor(resuscitation.procedures, levels = unique(resuscitation.procedures))
+    
+    ##Requires library(labelled)
+    var_label(data) <- list(
+        ofi = "Opportunity for improvement",
+        ed_gcs_sum = "GCS",
+        ed_sbp_value = "Systolic Blood Pressure",
+        pt_age_yrs = "Age",
+        ed_rr_value = "Respiratory rate",
+        ISS = "Injury severity score (ISS)",
+        pt_Gender = "Gender",
+        resuscitation.procedures = "Resuscitation procedure",
+        res_survival = "Mortality (within 30 days)"
+    ) 
 
-##Requires library(labelled)
-var_label(data) <- list(
-  ofi = "Opportunity for improvement",
-  ed_gcs_sum = "GCS",
-  ed_sbp_value = "Systolic Blood Pressure",
-  pt_age_yrs = "Age",
-  ed_rr_value = "Respiratory rate",
-  ISS = "Injury severity score (ISS)",
-  pt_Gender = "Gender",
-  ed_emerg_proc ="Emergency procedure",
-  ed_emerg_proc_other ="OTHER INTERVENTIONS",
-  res_survival = "Mortality (within 30 days)"
-) 
-
-data_format <- data
-return(data_format)
+    data_format <- data
+    return(data_format)
 
 }
 
--- table_2.R
+++ table_2.R
@@ -1,21 +1,11 @@
 source("main.R")
-library(gtsummary)
-
-# Create a data frame with the variables of interest, 
-df <- clean_variables
+library(broom)
 
 # create two models
-model1 <- glm(ofi ~ pt_Gender + res_survival + pt_age_yrs + ed_sbp_value + ed_rr_value + ed_gcs_sum + ISS, data = df, family = binomial())
-model2 <- glm(ofi ~ ed_emerg_proc_other, data = df, family = binomial())
-model3 <- glm(ofi ~ ed_emerg_proc, data = df, family = binomial())
+model1 <- glm(ofi ~ pt_Gender + res_survival + pt_age_yrs + ed_sbp_value + ed_rr_value + ed_gcs_sum + ISS + resuscitation.procedures, data = study.dataset, family = binomial())
 
-model1_table <- tbl_regression(model1)
-model1_table
-model2_table <- tbl_regression(model2)
-model2_table
-model3_table <- tbl_regression(model3)
-model3_table
-
+model1.table <- tidy(model1, conf.int = TRUE)
+saveRDS(model1.table, "model1-table.Rds")
 ## combine the two models using rbind()
 #combined_model <- cbind(summary(model1)$coefficients, summary(model2)$coefficients, summary(model3)$coefficients)
 
