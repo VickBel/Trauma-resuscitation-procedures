24bbfbb5cf9f48926b47b1efa3f73833598890f0
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2023-04-10 21:13:12 +0200

    Create regression table

--- table_2.R
+++ table_2.R
@@ -1,13 +1,10 @@
-source("main.R")
-library(broom)
-
 # create two models
 model1 <- glm(ofi ~ pt_Gender + res_survival + pt_age_yrs + ed_sbp_value + ed_rr_value + ed_gcs_sum + ISS + resuscitation.procedures, data = study.dataset, family = binomial())
 
-model1.table <- tidy(model1, conf.int = TRUE) %>% select(term, estimate, conf.low, conf.high, p.value)
-model1.table[, c("estimate", "conf.low", "conf.high")] <- exp(model1.table[, c("estimate", "conf.low", "conf.high")])
-model1.table[, c("estimate", "conf.low", "conf.high", "p.value")] <- lapply(model1.table[, c("estimate", "conf.low", "conf.high", "p.value")], function(x) sprintf("%.3f", x))
+model1.table <- as_kable(tbl_regression(model1, exponentiate = TRUE))
 saveRDS(model1.table, "model1-table.Rds")
+write(model1.table, "table2.Rmd")
+rmarkdown::render("table2.Rmd", output_format = "word_document")
 ## combine the two models using rbind()
 #combined_model <- cbind(summary(model1)$coefficients, summary(model2)$coefficients, summary(model3)$coefficients)
 
