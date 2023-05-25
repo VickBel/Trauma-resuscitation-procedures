dd3c1226ff1ff6740b94e64c3ac053b26d26d35e
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2023-05-25 22:39:56 +0200

    Update table 1

--- table_1.R
+++ table_1.R
@@ -1,26 +1,30 @@
 
-# Get format for variables
+## Get format for variables
 study.dataset <- format_table(study.dataset)
 
-# Table stratified by ofi.
+## Table stratified by ofi.
 study.dataset$ofi <- factor(study.dataset$ofi, levels = c("Yes", "No"), labels = c("OFI", "No OFI"))
 
-## create raw table 1 file
-table1 <-
-    as.data.frame(table1(
-        ~ pt_age_yrs +
-            pt_Gender +
-            ISS +
-            ed_rr_value +
-            ed_gcs_sum +
-            ed_sbp_value +
-            resuscitation.procedures +
-            res_survival
-    | ofi,
-    data = study.dataset,
-    caption = "Demographic and Clinical Characteristics of patients screened for OFI."
-    ))
+## Create table 1
+table1 <- study.dataset %>%
+    select(pt_age_yrs, 
+           pt_Gender, 
+           ISS, 
+           ed_rr_value, 
+           ed_gcs_sum, 
+           ed_sbp_value, 
+           resuscitation.procedures, 
+           res_survival,
+           ofi) %>%
+    tbl_summary(by = "ofi") %>%
+    add_p(test.args = all_categorical() ~ list(simulate.p.value = TRUE)) %>%
+    add_overall() %>%
+    bold_labels %>%
+    as_tibble() %>%
+    replace(is.na(.), "")
 
+
+## Save to disk and compile as word document
 saveRDS(table1, "table1.Rds")
 write(kable(table1, format = "pandoc"), "table1.Rmd")
 rmarkdown::render("table1.Rmd", output_format = "word_document")
